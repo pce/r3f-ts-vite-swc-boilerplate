431e322ec6a700eb7d7a21792ab4a2019749294b
pce <info@pc-e.org>
2023-11-28 00:42:17 +0100

    feat: gltf loader

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,27 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Stats, OrbitControls, Circle } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
 import "./app.scss";
-import CamAnimation from "./animations/cam-animation";
-import Moon from "./moon";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 function App() {
-
+  const gltf = useLoader(GLTFLoader, "./models/ganesha.glb");
 
   return (
     <div className="App">
       <div className="CanvasWrapper">
-        <Canvas camera={{ position: [-20, -10, 5] }} shadows={true}>
-          {/* <ambientLight/> */}
-          <pointLight position={[-10, -10, -10]} />
-          <Moon pos={[0, 0, 0]} />
-          <OrbitControls />
-          <CamAnimation/>
+        <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+          <directionalLight position={[3.3, 1.0, 4.4]} castShadow />
+          <primitive
+            object={gltf.scene}
+            position={[0, 1, 0]}
+            children-0-castShadow
+          />
+          <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+            <meshStandardMaterial />
+          </Circle>
+          <OrbitControls target={[0, 1, 0]} />
+          {/* <axesHelper args={[5]} /> */}
+          <Stats />
         </Canvas>
       </div>
     </div>
